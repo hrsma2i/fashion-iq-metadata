110a7aacddbe53ff880fa9d203a338a7f21c44e7
hiroshi.matsui <hiroshi.matsui@STTA18006-Hiroshi-Matsui.local>
2019-08-18 17:52:41 +0900

    Concatenate all url files as a file

--- README.md
+++ README.md
@@ -10,16 +10,19 @@ Excute the following script while there are images failed to download.
 
 ```sh
 ./retry_dl_img.sh
+```
+
+If images have been downloaded, execute the following commands.
+
+```sh
 ./compensate_for_broken_links.sh
+git clone https://github.com/XiaoxiaoGuo/fashion-iq.git raw/fashion-iq
+rm -rf raw/fashion-iq/.git
+python raw/fashion-iq/start_kit/resize_images.py \
+    --image_dir raw/images \
+    --output_dir raw/resized_images
 ```
 
 ```sh
 ./make_main.sh
-```
-
-```sh
-git clone https://github.com/XiaoxiaoGuo/fashion-iq.git raw/fashion-iq
-rm -rf raw/fashion-iq/.git
-mkdir labels
-cd labels
 ```
--- retry_dl_img.sh
+++ retry_dl_img.sh
@@ -1,28 +1,10 @@
 RAW_DIR="${DATASET_ROOT}/raw"
 IMAGE_URL_DIR="${RAW_DIR}/image_url"
-if [ -e ${IMAGE_URL_DIR}/dress_error_urls.tsv ]; then
-    cat ${IMAGE_URL_DIR}/dress_error_urls.tsv | sort | uniq > ${IMAGE_URL_DIR}/dress_error_urls_pre.tsv;
-    /bin/rm ${IMAGE_URL_DIR}/dress_error_urls.tsv;
-    aiodl  ${IMAGE_URL_DIR}/dress_error_urls_pre.tsv \
+if [ -e ${IMAGE_URL_DIR}/error_urls.tsv ]; then
+    cat ${IMAGE_URL_DIR}/error_urls.tsv | sort | uniq > ${IMAGE_URL_DIR}/error_urls_pre.tsv;
+    /bin/rm ${IMAGE_URL_DIR}/error_urls.tsv;
+    aiodl  ${IMAGE_URL_DIR}/error_urls_pre.tsv \
         -r 50 -t 300 -c -f -d "\t" \
-        -o ${RAW_DIR}/dress_images \
-        -e ${IMAGE_URL_DIR}/dress_error_urls.tsv;
-fi;
-
-if [ -e ${IMAGE_URL_DIR}/shirt_error_urls.tsv ]; then
-    cat ${IMAGE_URL_DIR}/shirt_error_urls.tsv | sort | uniq > ${IMAGE_URL_DIR}/shirt_error_urls_pre.tsv;
-    /bin/rm ${IMAGE_URL_DIR}/shirt_error_urls.tsv;
-    aiodl  ${IMAGE_URL_DIR}/shirt_error_urls_pre.tsv \
-        -r 50 -t 300 -c -f -d "\t" \
-        -o ${RAW_DIR}/shirt_images \
-        -e ${IMAGE_URL_DIR}/shirt_error_urls.tsv;
-fi;
-
-if [ -e ${IMAGE_URL_DIR}/toptee_error_urls.tsv ]; then
-    cat ${IMAGE_URL_DIR}/toptee_error_urls.tsv | sort | uniq > ${IMAGE_URL_DIR}/toptee_error_urls_pre.tsv;
-    /bin/rm ${IMAGE_URL_DIR}/toptee_error_urls.tsv;
-    aiodl  ${IMAGE_URL_DIR}/toptee_error_urls_pre.tsv \
-        -r 50 -t 300 -c -f -d "\t" \
-        -o ${RAW_DIR}/toptee_images \
-        -e ${IMAGE_URL_DIR}/toptee_error_urls.tsv;
+        -o ${RAW_DIR}/images \
+        -e ${IMAGE_URL_DIR}/error_urls.tsv;
 fi;
